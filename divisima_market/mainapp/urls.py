2c971e1a25ab8d3180c0d2391a652b74d52cd823
Alex <alexlisecky@gmail.com>
2021-12-27 23:39:33 +0300

    prepared templates

--- divisima_market/mainapp/urls.py
+++ divisima_market/mainapp/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import Index
+from .views import IndexView, CartView, CategoryView, CheckoutView, ContactView, ProductView
 
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', Index.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('category/', CategoryView.as_view(), name='category'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('product/', ProductView.as_view(), name='product')
 ]
--- divisima_market/mainapp/views.py
+++ divisima_market/mainapp/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
-class Index(ListView):
+class IndexView(ListView):
     template_name = 'mainapp/index.html'
+    queryset = []
+
+
+class ProductView(ListView):
+    template_name = 'mainapp/product.html'
+    queryset = []
+
+
+class ContactView(ListView):
+    template_name = 'mainapp/contact.html'
+    queryset = []
+
+
+class CheckoutView(ListView):
+    template_name = 'mainapp/checkout.html'
+    queryset = []
+
+
+class CategoryView(ListView):
+    template_name = 'mainapp/category.html'
+    queryset = []
+
+
+class CartView(ListView):
+    template_name = 'mainapp/cart.html'
     queryset = []

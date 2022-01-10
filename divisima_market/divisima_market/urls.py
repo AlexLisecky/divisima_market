e20529b195941c6448692a0fc3f1eb62c9502503
Alex <alexlisecky@gmail.com>
2022-01-10 15:47:33 +0300

    add authapp-v.2

--- divisima_market/divisima_market/urls.py
+++ divisima_market/divisima_market/urls.py
@@ -22,7 +22,8 @@ from mainapp.views import pageNotFound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainapp.urls', namespace='main'))
+    path('', include('mainapp.urls', namespace='main')),
+    path('auth', include('authapp.urls', namespace='auth'))
 ]
 
 handler404 = pageNotFound

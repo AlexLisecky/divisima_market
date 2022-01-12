6014bb9241f479628c44ea8f76bb227c2a2068e2
Alex <alexlisecky@gmail.com>
2022-01-12 19:37:19 +0300

    add user market

--- divisima_market/authapp/models.py
+++ divisima_market/authapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class UserMarket()
+
+class UserMarket(AbstractUser):
+    email = models.EmailField(verbose_name='Почта', unique=True)
+    avatar = models.ImageField(verbose_name='Аватар', blank=True)
